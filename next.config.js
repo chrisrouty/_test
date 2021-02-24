5d16948b617f5e112a928cc300a35679c2337f29
Chris Routy <chrisrouty@gmail.com>
2021-02-24 10:13:09 +0100

    [continue development...]

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    productionBrowserSourceMaps: true,
     async redirects() {
         return [
             {
@@ -11,4 +12,5 @@ module.exports = {
     env: {
         DATABASE_URL: 'postgres://dbvovietnam:postgres@localhost:5432/dbvovietnam?schema=public',
     },
+
 }
